395fffc0321d65fd46d856f1008e8015ea4e34f1
Lelosz <milosz0987654321_1990@o2.pl>
2023-11-11 00:01:56 +0100

    added login and signup views

---
+++ VueApp/src/components/LogIn.vue
@@ -0,0 +1,46 @@
+﻿<script>
+    export default {
+        data: () => ({
+            login: '',
+            loginRules: [
+                value => {
+                    if (value?.length > 3) return true
+
+                    return 'Nazwa użytkownika powinna miec conajmniej 4 znaki'
+                },
+            ],
+            password: '',
+            passwordRules: [
+                value => {
+                    if (value?.length > 7) return true
+
+                    return 'Hasło powinno miec conajmniej 8 znaków'
+                },
+            ],
+        }),
+    }
+</script>
+
+<template>
+    <v-sheet width="500" class="mx-auto">
+        <v-form @submit.prevent="submit">
+            <v-text-field
+                v-model="login"
+                label="Nazwa użytkownika"
+                :rules="loginRules"
+            ></v-text-field>
+
+            <v-text-field
+                v-model="password"
+                type="password"
+                label="Hasło"
+                :rules="passwordRules"
+            ></v-text-field>
+
+            <v-btn type="submit">Zaloguj się</v-btn>
+            <router-link to="/SignUp">
+                <v-btn>Zarejestruj się</v-btn>
+            </router-link>
+</v-form>
+    </v-sheet>
+</template>
---
+++ VueApp/src/components/SignUp.vue
@@ -0,0 +1,45 @@
+﻿<script>
+    export default {
+        data: () => ({
+            login: '',
+            loginRules: [
+                value => {
+                    if (value?.length > 3) return true
+
+                    return 'Nazwa użytkownika powinna miec conajmniej 4 znaki'
+                },
+            ],
+            password: '',
+            passwordRules: [
+                value => {
+                    if (value?.length > 7) return true
+
+                    return 'Hasło powinno miec conajmniej 8 znaków'
+                },
+            ],
+            repeatPassword:'',
+        }),
+    }
+</script>
+
+<template>
+    <v-sheet width="500" class="mx-auto">
+        <v-form @submit.prevent="submit">
+            <v-text-field v-model="login"
+                          label="Nazwa użytkownika"
+                          :rules="loginRules"></v-text-field>
+
+            <v-text-field v-model="password"
+                          type="password"
+                          label="Hasło"
+                          :rules="passwordRules"></v-text-field>
+
+            <v-text-field v-model="repeatPassword"
+                          type="password"
+                          label="Powtórz hasło"
+                          :rules="passwordRules"></v-text-field>
+
+            <v-btn type="submit">Zarejestruj się</v-btn>
+        </v-form>
+    </v-sheet>
+</template>
